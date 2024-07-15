6748cc6d4d51457efeccbcc3cfdeac0086f02258
Abhi Nivesh R <abhinivesh382002@gmail.com>
2024-07-15 23:27:09 +0530

    configure tomcat server

--- ems-backend/src/main/java/com/project/emsbackend/EmsBackendApplication.java
+++ ems-backend/src/main/java/com/project/emsbackend/EmsBackendApplication.java
@@ -2,12 +2,17 @@ package com.project.emsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class EmsBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmsBackendApplication.class, args);
 	}
 
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(EmsBackendApplication.class);
+	}
 }
